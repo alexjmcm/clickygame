409ce27b651d71120ca982b71d9a3d3629b48901
Alex Monteiro <alexjmcm@Alexs-MacBook-Pro.local>
2018-12-06 19:56:46 -0500

    Updates

---
+++ precache-manifest.a153644fdf9db0a97323fb6780d8fd6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "44b9454a6692e848b9bf",
+    "url": "/clickygame/static/js/main.44b9454a.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/clickygame/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "44b9454a6692e848b9bf",
+    "url": "/clickygame/static/css/main.e08434b7.chunk.css"
+  },
+  {
+    "revision": "59657c7dbb51ea0b19c7ac21ddf432d8",
+    "url": "/clickygame/index.html"
+  }
+];
